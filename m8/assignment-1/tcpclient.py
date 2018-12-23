1e1386e93be16ad3c7f86a43d57344349a4bc92d
ravalishnuguri <ravalishnuguri@msitprogram.net>
2018-12-23 15:06:18 +0530

    moudle-8

---
+++ m8/assignment-1/tcpclient.py
@@ -0,0 +1,18 @@
+import socket
+
+def main():
+	host = '10.1.132.80'
+	port = 4202
+
+	s = socket.socket()
+	s.connect((host,port))
+	message = input("enter message: ")
+	while message!='q':
+		s.send(message.encode())
+		data = s.recv(1024)
+		print("recieved message: "+str(data.decode()))
+		message = input("enter message: ")
+	s.close()
+if __name__ == "__main__":
+	main()	
+
